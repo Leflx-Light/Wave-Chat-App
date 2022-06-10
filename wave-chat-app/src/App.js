9751606ecda6a1f1641f5f86e4f2dd86b8f7ee1c
SONU <leflxlight@gmail.com>
2022-06-10 12:08:47 +0530

    Scaffold the wave-chat-app

---
+++ wave-chat-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'rsuite/dist/rsuite.min.css';
+import './styles/main.scss';
+function App() {
+  return (
+    <div>
+      <p> Hello World</p>
+    </div>
+  );
+}
+
+export default App;
